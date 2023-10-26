5ca54faa6a5d0bdb3395134368681a393f2da33f
RobsonMCambraia <robson.mcambraia23@gmail.com>
2023-10-26 14:22:07 -0300

    "adicionei novos inimigos"

--- game-robson.py
+++ game-robson.py
@@ -35,6 +35,7 @@ score_pen.hideturtle()
 
 # Registrar as formas
 turtle.register_shape("invader.gif")
+turtle.register_shape("enemy1.gif")
 turtle.register_shape("player (1).gif")
 
 # Criar a tartaruga do jogador
@@ -47,28 +48,42 @@ player.setheading(90)
 
 playerspeed = 15
 
-number_of_enemies = 10
-enemies = []
+number_of_enemies1 = 3
+number_of_enemies2 = 7
+
+enemies1 = []
+enemies2 = []
+
 
 # Adicionar inimigos à lista
-for i in range(number_of_enemies):
-    enemies.append(turtle.Turtle())
+for i in range(number_of_enemies1):
+    enemies1.append(turtle.Turtle())
+for i in range(number_of_enemies2):
+    enemies2.append(turtle.Turtle())
 
 # Configuração dos inimigos
-for enemy in enemies:
-    enemy.shape("invader.gif")
-    enemy.penup()
-    enemy.speed(0)
+for enemy1 in enemies1:
+    enemy1.shape("invader.gif")
+    enemy1.penup()
+    enemy1.speed(0)
     x = random.randint(-200, 200)
     y = random.randint(100, 250)
-    enemy.setposition(x, y)
-
+    enemy1.setposition(x, y)
+    
+for enemy2 in enemies2:
+    enemy2.shape("enemy1.gif")
+    enemy2.penup()
+    enemy2.speed(0)
+    x = random.randint(-200, 200)
+    y = random.randint(100, 250)
+    enemy2.setposition(x, y)
+    
 enemyspeed = 5
 
 # Configuração do projétil disparado pelo jogador
 fogo = turtle.Turtle()
-fogo.color("yellow")
-fogo.shape("triangle")
+fogo.color("dark orange")
+fogo.shape("circle")
 fogo.penup()
 fogo.speed(0.1)
 fogo.setheading(90)
@@ -109,21 +124,38 @@ turtle.onkey(mover_direita, "Right")
 turtle.onkey(mover_fogo, "space")
 
 while i < 10:
-    for enemy in enemies:
-        x = enemy.xcor()
+    for enemy1 in enemies1:
+        x = enemy1.xcor()
         x += enemyspeed
-        enemy.setx(x)
+        enemy1.setx(x)
 
         # Verifique se o inimigo atingiu a borda
         if x > 280:
-            y = enemy.ycor()
+            y = enemy1.ycor()
             y -= 20
-            enemy.sety(y)
+            enemy1.sety(y)
             enemyspeed *= -1
         if x < -280:
-            y = enemy.ycor()
+            y = enemy1.ycor()
             y -= 20
-            enemy.sety(y)
+            enemy1.sety(y)
             enemyspeed *= -1
+            
+    for enemy2 in enemies2:
+        x = enemy2.xcor()
+        x += enemyspeed
+        enemy2.setx(x)
 
+        # Verifique se o inimigo atingiu a borda
+        if x > 280:
+            y = enemy2.ycor()
+            y -= 20
+            enemy2.sety(y)
+            enemyspeed *= -1
+        if x < -280:
+            y = enemy2.ycor()
+            y -= 20
+            enemy2.sety(y)
+            enemyspeed *= -1
+            
 turtle.done()
